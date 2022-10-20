967611564b40b60266205756472cc26cfb20d83b
Luis Velásquez <luvejo.dev@gmail.com>
2022-10-19 19:46:54 -0500

    feat: add initial commit with minimal Nuxt app

---
+++ app.vue
@@ -0,0 +1,14 @@
+<script setup>
+const res = await useFetch(
+  "https://strangerthings-quotes.vercel.app/api/quotes/5",
+  {
+    method: "get",
+  }
+);
+</script>
+
+<template>
+  <div>
+    {{ res?.data }}
+  </div>
+</template>
